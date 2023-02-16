fed21c644f735de27b89d7e707f1bbcbcdb59097
lomar92 <amarlojo@gmail.com>
2023-02-16 08:56:39 +0100

    policy

--- policies.tf
+++ policies.tf
@@ -5,9 +5,3 @@ resource "vault_policy" "k8s_policy" {
   namespace = vault_namespace.kunde1.path
 }
 
-resource "vault_policy" "user" {
-  name      = "user_policy"
-  policy    = file("user-policy.hcl")
-  namespace = vault_namespace.kunde1.path
-
-}
